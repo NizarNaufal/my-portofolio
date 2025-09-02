477ed3c645284e02832208bb4379ea10129e019b
Nizar Naufal <nizar.naufal@digitalplace.ai>
2025-09-02 15:25:16 +0700

    Fix: Add Kreditplus Mobile Apps, Osa Apps

--- src/data/articles.js
+++ src/data/articles.js
@@ -2,6 +2,127 @@ import React from "react";
 
 function article_1() {
 	return {
+		date: "March 2025",
+		title: "Flutter and Dart: Developing iOS, Android, and Mobile Apps",
+		description:
+			"IBM iX",
+		keywords: [
+			"The Benefits of Cloud Computing",
+			"Tharindu",
+			"Tharindu N",
+			"Tharindu Nayanajith",
+		],
+		style: `
+				.article-content {
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+				}
+
+				.randImage {
+					align-self: center;
+					outline: 2px solid red;
+				}
+				`,
+		body: (
+			<React.Fragment>
+				<div className="article-content">
+					<div className="paragraph">Content of article 1</div>
+					<img
+						src="https://picsum.photos/200/300"
+						alt="random"
+						className="randImage"
+					/>
+				</div>
+			</React.Fragment>
+		),
+	};
+}
+
+function article_2() {
+	return {
+		date: "March 2025",
+		title: "Software Design and Architecture Specialization",
+		description:
+			"University of Alberta.",
+		style: ``,
+		keywords: [
+			"Artificial Intelligence in Healthcare",
+			"Tharindu",
+			"Tharindu N",
+			"Tharindu Nayanajith",
+		],
+		body: (
+			<React.Fragment>
+				<h1>Content of article 2</h1>
+			</React.Fragment>
+		),
+	};
+}
+
+
+function article_3() {
+	return {
+		date: "March 2021",
+		title: "Software Design as an Element of the Software Development Lifecycle ",
+		description:
+			"University of Colorado.",
+		keywords: [
+			"The Benefits of Cloud Computing",
+			"Tharindu",
+			"Tharindu N",
+			"Tharindu Nayanajith",
+		],
+		style: `
+				.article-content {
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+				}
+
+				.randImage {
+					align-self: center;
+					outline: 2px solid red;
+				}
+				`,
+		body: (
+			<React.Fragment>
+				<div className="article-content">
+					<div className="paragraph">Content of article 1</div>
+					<img
+						src="https://picsum.photos/200/300"
+						alt="random"
+						className="randImage"
+					/>
+				</div>
+			</React.Fragment>
+		),
+	};
+}
+
+function article_4() {
+	return {
+		date: "March 2025",
+		title: "Software Engineering: Software Design and Project Management",
+		description:
+			"The Hong Kong University of Science and Technology.",
+		style: ``,
+		keywords: [
+			"Artificial Intelligence in Healthcare",
+			"Tharindu",
+			"Tharindu N",
+			"Tharindu Nayanajith",
+		],
+		body: (
+			<React.Fragment>
+				<h1>Content of article 2</h1>
+			</React.Fragment>
+		),
+	};
+}
+
+function article_5() {
+	return {
 		date: "July 2021",
 		title: "Configuration Management and the Cloud - Part of our Google IT Automation with Python Professional",
 		description:
@@ -39,7 +160,7 @@ function article_1() {
 	};
 }
 
-function article_2() {
+function article_6() {
 	return {
 		date: "July 2021",
 		title: "Foundation: Data, Data, Everywhere - Part Of Our Google Data Analytics",
@@ -61,7 +182,7 @@ function article_2() {
 }
 
 
-function article_3() {
+function article_7() {
 	return {
 		date: "July 2021",
 		title: "Crash Course On Python - Part of our Google IT Automation with Python ProfessionalFoundation: Data, Data, Everywhere - Part Of Our Google Data Analytics",
